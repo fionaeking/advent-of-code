bf000faebfa47eee03fe7a6a18e26ed4147bf077
Fiona King <Fiona.King@metaswitch.com>
2020-04-13 13:12:37 +0100

    Refactor Intcode computer

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using System.IO;
 using System;
 
 namespace Day7
@@ -9,10 +10,10 @@ namespace Day7
         static void Main(string[] args)
         {
             int outputValue = 0;
-            var puzzleInput = new List<int>(){3,8,1001,8,10,8,105,1,0,0,21,42,67,84,109,122,203,284,365,446,99999,3,9,1002,9,3,9,1001,9,5,9,102,4,9,9,1001,9,3,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,1001,9,4,9,102,3,9,9,101,3,9,9,4,9,99,3,9,101,5,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,102,5,9,9,101,5,9,9,102,3,9,9,101,3,9,9,102,2,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99};
+            var puzzleInput = puzzleInputToList(Constants.INPUT_FILENAME);
             int max = 0;
-            IEnumerable<int> phaseSequenceMax = new int[]{};
-            
+            IEnumerable<int> phaseSequenceMax = new int[] { };
+
             //var phaseSequence = new List<int>(){0,1,2,3,4};
             IEnumerable<IEnumerable<int>> result =
                 getCombinations(Enumerable.Range(0, 5), 5);
@@ -30,9 +31,16 @@ namespace Day7
                     phaseSequenceMax = phaseSequence;
                 }
             }
-            
+
             Console.WriteLine(max);
             Console.WriteLine(String.Join(',', phaseSequenceMax));
+        }
+
+        static List<int> puzzleInputToList(string inputFilePath)
+        {
+            var str = File.ReadLines(inputFilePath).First();
+            var listOfInts = str.Split(',').Select(int.Parse).ToList();
+            return listOfInts;
         }
 
         //TODO/Confession - Took this function off StackOverflow without understanding yet
@@ -44,6 +52,6 @@ namespace Day7
                 .SelectMany(t => list.Where(e => !t.Contains(e)),
                     (t1, t2) => t1.Concat(new T[] { t2 }));
         }
-        
+
     }
 }
