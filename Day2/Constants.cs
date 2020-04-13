615f47bdfbb4207dd9a7b4d41c8661679e6cc9ba
Fiona King <Fiona.King@metaswitch.com>
2020-04-13 11:58:16 +0100

    Add comments

---
+++ Day2/Constants.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+class Constants
+{
+    public const string INPUT_FILENAME = "InputList.csv";
+
+}
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;  
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+
+// Intcode computer 
 
 namespace Day2
 {
@@ -8,69 +11,64 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            var puzzleInput = new List<int>(){1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,6,23,27,1,6,27,31,2,31,9,35,1,35,6,39,1,10,39,43,2,9,43,47,1,5,47,51,2,51,6,55,1,5,55,59,2,13,59,63,1,63,5,67,2,67,13,71,1,71,9,75,1,75,6,79,2,79,6,83,1,83,5,87,2,87,9,91,2,9,91,95,1,5,95,99,2,99,13,103,1,103,5,107,1,2,107,111,1,111,5,0,99,2,14,0,0};
+            var puzzleInput = puzzleInputToList(Constants.INPUT_FILENAME);
             int instructionPointer = 0;
             var instructionLength = checkInstruction(puzzleInput[0]);
-            while (instructionLength!=0)
+            while (instructionLength != 0)
             {
                 var instructionValues = getInputValues(puzzleInput, instructionPointer, instructionLength);
                 performInstruction(instructionValues, puzzleInput);
                 instructionPointer += instructionLength;
-                instructionLength = checkInstruction(puzzleInput[0+instructionPointer]);
+                instructionLength = checkInstruction(puzzleInput[0 + instructionPointer]);
             }
-            
-            Console.WriteLine("Printing out puzzle output");
-            Console.WriteLine(string.Join(",", puzzleInput.ToArray()));
-            return; 
+            printOutput(puzzleInput);
+        }
+
+        static List<int> puzzleInputToList(string inputFilePath)
+        {
+            var str = File.ReadLines(inputFilePath).First();
+            var listOfInts = str.Split(',').Select(int.Parse).ToList();
+            return listOfInts;
         }
 
         static List<int> getInputValues(List<int> inputNumList, int offset, int length)
         {
             var inputValues = new List<int>();
-            for (int i=0; i<length; i++)
+            for (int i = 0; i < length; i++)
             {
-                inputValues.Add(inputNumList[i+offset]);
+                inputValues.Add(inputNumList[i + offset]);
             }
             return inputValues;
         }
-        
+
         static int checkInstruction(int opcode)
         {
-            switch(opcode)
+            //Return length of instruction
+            switch (opcode)
             {
-                case 1:
-                return 4;
-
-                case 2:
-                return 4;
-                
-                case 99:
-                Console.WriteLine("End of program");
-                return 0;
-
-                default:
-                Console.WriteLine("Error - terminating program");
-                return 0;
+                case 1: return 4;
+                case 2: return 4;
+                case 99: return 0;
+                default: throw new Exception("Error - unrecognised opcode");
             }
         }
         static void performInstruction(List<int> instructionValues, List<int> puzzleInput)  //List<int>
         {
-            int outputValue, firstInt, secondInt;
-            switch(instructionValues[0])
+            int firstInt, secondInt;
+            var opcode = instructionValues[0];
+            switch (opcode)
             {
                 case 1:  // Addition
                     firstInt = getValueFromPosn(puzzleInput, instructionValues[1]);
                     secondInt = getValueFromPosn(puzzleInput, instructionValues[2]);
-                    outputValue = firstInt + secondInt;
                     // Note to self - using list mutability
-                    puzzleInput[instructionValues[3]] = Convert.ToInt32(outputValue);
-                    return;
+                    puzzleInput[instructionValues[3]] = firstInt + secondInt;
+                    break;
                 case 2:  // Multiplication
                     firstInt = getValueFromPosn(puzzleInput, instructionValues[1]);
                     secondInt = getValueFromPosn(puzzleInput, instructionValues[2]);
-                    outputValue = firstInt * secondInt;
-                    puzzleInput[instructionValues[3]] = Convert.ToInt32(outputValue);
-                    return;
+                    puzzleInput[instructionValues[3]] = firstInt * secondInt;
+                    break;
                 default:
                     throw new Exception("Unrecognised input");
             }
@@ -79,6 +77,12 @@ namespace Day2
         static int getValueFromPosn(List<int> inputNums, int input)
         {
             return inputNums[input];
+        }
+
+        static void printOutput(List<int> puzzleOutput)
+        {
+            Console.WriteLine("End of program. Printing out puzzle output:");
+            Console.WriteLine(string.Join(",", puzzleOutput.ToArray()));
         }
 
     }
