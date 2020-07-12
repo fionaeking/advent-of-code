729987651d75e39829ce0ea16429c7f2a7dd9267
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-07-12 22:35:34 +0100

    Update Day4

--- Day5/Intcode.cs
+++ Day5/Intcode.cs
@@ -64,14 +64,14 @@ class Intcode
         // Return length of instruction
         switch (opcode)
         {
-            case 1: return 4;
-            case 2: return 4;
-            case 3: return 2;
-            case 4: return 2;
-            case 5: return 3;
-            case 6: return 3;
-            case 7: return 4;
+            case 1:
+            case 2: 
+            case 7: 
             case 8: return 4;
+            case 3:
+            case 4: return 2;
+            case 5: 
+            case 6: return 3;
             case 99: return 0;
             default: throw new Exception("Error - unrecognised opcode");
         }
